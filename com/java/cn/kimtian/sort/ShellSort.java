fcadbabb419186ad15d057e3e9262f952c8f9ee5
田金江 <kimtian@tianjinjiangdeMacBook-Air.local>
2019-01-08 15:13:04 +0800

    this is a sort add

---
+++ com/java/cn/kimtian/sort/ShellSort.java
@@ -0,0 +1,21 @@
+package cn.kimtian.sort;
+
+/**
+ * 这是一个希尔排序
+ *
+ * @author kimtian
+ */
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,6,8,9,3,2,0,3};
+        shellSort(arr);
+    }
+    /**
+     * 这是一个希尔排序
+     * @param arr 数组
+     */
+    public static void shellSort(int[] arr ){
+
+
+    }
+}
